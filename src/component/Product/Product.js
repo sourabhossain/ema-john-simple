43fabd50c50d6c2e54e469460a2c0fa33d5e77f2
Sourab Hossain <sourabhossain.fci@gmail.com>
2021-03-16 09:34:36 +0600

    FontAwesomeIcon

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Product = (props) => {
 
-    console.log(props.product);
+    const {img, name, seller, price, stock} = props.product;
 
     return (
-        <div>
-            
+        <div className="product">
+            <div>
+                <img src={img} alt={name}/>
+            </div>
+
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br/>
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <br/>
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart} /> Add to cart</button>
+            </div>
         </div>
     );
 };
